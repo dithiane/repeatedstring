62aa93b5589b46396a186262d7c9834022fe70aa
emenaTechtonic <menaelena.littleton@gmail.com>
2020-04-16 18:04:26 -0600

    Solved

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
+	str := "a"
+	n := 1000000000000
+	lengthOfWhole := n / len(str)
+	numberOfLeftsymbols := n - lengthOfWhole*len(str)
+	aOccurences := strings.Count(str, "a")
+	sliceOfLeft := str[0:numberOfLeftsymbols]
+	totala := aOccurences*lengthOfWhole + strings.Count(sliceOfLeft, "a")
+	fmt.Println(totala)
 
 }
